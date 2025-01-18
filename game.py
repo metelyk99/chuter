bfdfcc80dfea233d5ffc85d6b70636a29301df85
metelyk99 <mukutykartem26@gmail.com>
2025-01-18 11:33:58 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -1,47 +1,63 @@
-from pygame import *
- 
-# фонова музика
+
+from pygame import*
+from random import randint
+
 mixer.init()
 mixer.music.load('begraund_myzik.ogg')
-mixer.music.play()
+mixer_music.play()
 fire_sound = mixer.Sound('chut.ogg')
- 
-# нам потрібні такі картинки:
-img_back = "begraund.jpg.jpg"  # фон гри
-img_hero = "sprite.jpg"  # герой
+
+font.init()
+font1 = font.Font(None, 80)
+font2 = font.Font(None, 36)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+
+font.init()
+font2 = font.Font(None, 36)
+
+img_back = "begraund.jpg.jpg"
+img_hero = "sprite.jpg"
+img_enemy = "inoplenitian.jpg"
+img_bullet = "pyla.png"
 
 clock = time.Clock()
 FPS = 60
 
-# клас-батько для інших спрайтів
-class GameSprite(sprite.Sprite):
-    # конструктор класу
-    def __init__(
-            self,
-            player_image, 
-            player_x, 
-            player_y, 
-            size_x, 
-            size_y, 
-            player_speed
+score = 0 
+lost = 0
+max_lost = 3
+
+
+class GameSprite(sprite.Sprite):    
+    def __init__ (
+        self,
+        player_image,
+        player_x,
+        player_y,
+        size_x,
+        size_y,
+        player_speed
     ):
-        # викликаємо конструктор класу (Sprite):
         super().__init__()
-        # кожен спрайт повинен зберігати властивість image - зображення
         self.image = transform.scale(
             image.load(player_image), (size_x, size_y))
         self.speed = player_speed
- 
-        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
- 
-    # метод, що малює героя на вікні
+
     def reset(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
- 
-# клас головного гравця
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
 class Player(GameSprite):
  
     # метод для керування спрайтом стрілками клавіатури
@@ -54,8 +70,19 @@ class Player(GameSprite):
  
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
- 
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0 
+            lost = lost + 1
+
  
 # створюємо віконце
 win_width = 700
@@ -66,7 +93,15 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
  
 # створюємо спрайти
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(
+    80, win_width - 80), -40, 80, 50, randint(1, 3))
+    monsters.add(monster)
  
+bullets = sprite.Group()
+
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
  
@@ -78,16 +113,33 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+        elif  e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
  
     if not finish:
         # оновлюємо фон
         window.blit(background, (0, 0))
+
+        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 250))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render(f"Пропущено: {lost}", 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+         
  
         # рухи спрайтів
         ship.update()
- 
+        monsters.update()
+        bullets.update()
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
  
     display.update()
     clock.tick(FPS)
+
+
